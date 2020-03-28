01d5eb03501248cb965b4efb09738bc1bda47c93
Niek Nijland <ngnijland@gmail.com>
2020-03-28 14:14:56 +0100

    feat: change all default configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `When is the next leap year?`,
+    description: `This site shows when the next leap year is and why that year is a leap year.`,
+    author: `@nieknijland`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `When is the next leap year?`,
+        short_name: `Leap year`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#dcedf3`,
+        theme_color: `#dcedf3`,
+        display: `standalone`,
+        icon: `src/images/leapyear-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
