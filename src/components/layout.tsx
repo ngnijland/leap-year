6c29eb6a6e66d5b73fba8cbf65ec9f75f8773ff2
Niek Nijland <ngnijland@gmail.com>
2020-03-28 22:43:24 +0100

    feat: add twitter link

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,25 @@
 import React from "react"
 
 import layoutStyles from "./layout.module.css"
+import TwitterIcon from "./twitterIcon"
 
 export interface LayoutProps {
   children: JSX.Element | JSX.Element[]
 }
 
 function Layout({ children }: LayoutProps): JSX.Element {
-  return <main className={layoutStyles.root}>{children}</main>
+  return (
+    <>
+      <main className={layoutStyles.root}>{children}</main>
+      <a
+        className={layoutStyles.link}
+        href="https://twitter.com/nieknijland"
+        title="Created by Niek Nijland, link to Twitter"
+      >
+        <TwitterIcon width="32" height="32" />
+      </a>
+    </>
+  )
 }
 
 export default Layout
